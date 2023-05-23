4547643e4216363fa613d79450852d55b66d86bc
Yes a cringe name! <129857336+RomeandAlphaGuylol@users.noreply.github.com>
2023-05-23 15:27:20 +0530

    Delete SanaBO' directory

--- SanaBO'/triggers.py
+++
@@ -1,7 +0,0 @@
-class greetings:
-    def hi():
-        print("Well!, hello there!")
-    def howareyou():
-        print("I seem to be fine!")
-    def howislife():
-        print("Actually, with no bugs!")
